86a3d8861501b50f8242966e8d2cdbfc528c681c
Eric Ihle <zekeotronic@gmail.com>
2022-06-10 23:25:50 -0400

    add time function and reworked function structure

--- preload.js
+++ preload.js
@@ -1,19 +1,7 @@
-// preload.js
-
 const { contextBridge } = require('electron')
 const axios = require('axios').default
-
-// const urls = [
-//   {name: 'acjokes.com', url: 'https://www.acjokes.com'},
-//   {name: 'freezedrums.com', url: 'https://www.freezedrums.com'},
-//   {name: 'escosnaps.com', url: 'https://www.escosnaps.com'},
-//   {name: 'cabinasmanzanillo.com', url: 'https://www.cabinasmanzanillo.com'},
-//   {name: 'acjres.com', url: 'https://www.acjres.com'},
-//   {name: 'johnplate.com', url: 'https://www.johnplate.com'},
-//   {name: 'shnoice.com', url: 'https://www.shnoice.com'},
-//   {name: 'ericihle.com', url: 'https://www.ericihle.com'},
-// ]
-
+const x = './red-x.png';
+const check = './green-check.png';
 const statusCodes = [
   { 
     code: 100,
@@ -212,27 +200,70 @@ const statusCodes = [
     name: 'Network Authentication Required',
   }
 ]
-
-
+const createCard = (res, urlObj) => {
+  const url = urlObj
+  const statusCard = document.createElement('div');
+  const id = url.name.replace('.', '-');
+  statusCard.setAttribute('id', id);
+  statusCard.classList.add('status-card');
+  const status = res.status;
+  const statusName = statusCodes.find(code => code.code === status).name;
+  const siteName = url.name;
+  const siteUrl = url.url;
+  const time = res.end - res.start;
+  const topDiv = document.createElement('div');
+  topDiv.classList.add('top-div');
+  const img = document.createElement('img');
+  img.src = status === 200 ? check : x;
+  img.alt = statusName;
+  topDiv.appendChild(img);
+  const topText = document.createElement('p');
+  topText.classList.add('top-text');
+  textLink = document.createElement('a');
+  textLink.href = siteUrl;
+  textLink.innerText = siteName;
+  textLink.target = '_blank';
+  topText.appendChild(textLink);
+  topDiv.appendChild(topText);
+  const bottomDiv = document.createElement('div');
+  bottomDiv.classList.add('bottom-div');
+  const bottomText = document.createElement('p');
+  bottomText.classList.add('bottom-text');
+  bottomText.innerText = `${siteUrl} | ${status}-${statusName} | ${time}ms`;
+  bottomDiv.appendChild(bottomText)
+  const infoDiv = document.createElement('div');
+  infoDiv.classList.add('info-div');
+  const btnDiv = document.createElement('div');
+  btnDiv.classList.add('btn-div');
+  const deleteBtn = document.createElement('button');
+  deleteBtn.classList.add('delete-btn');
+  deleteBtn.innerText = 'Remove';
+  deleteBtn.addEventListener('click', () => {
+    localStorage.removeItem(siteName);
+    statusCard.remove();
+  })
+  btnDiv.appendChild(deleteBtn);
+  infoDiv.appendChild(topDiv);
+  infoDiv.appendChild(bottomDiv);
+  statusCard.appendChild(infoDiv);
+  statusCard.appendChild(btnDiv)
+  main.appendChild(statusCard);
+}
 const getStatus = async (urlObj) => {
-  const response = await axios.get(urlObj.url)
-  return response
+  let obj = {}
+  obj.start = getTime()
+  const response = await axios.get(urlObj.url).then(res => {
+    obj.status = res.status
+    obj.end = getTime()
+  })
+  return obj
   
 }
-
-
-
-// links the renderer process to the main process
-
-// contextBridge.exposeInMainWorld('results', results)
+const getTime = () => {
+  return new Date().getTime()
+}
+contextBridge.exposeInMainWorld('createCard', createCard)
 contextBridge.exposeInMainWorld('statusCodes', statusCodes)
-// contextBridge.exposeInMainWorld('urls', urls)
+contextBridge.exposeInMainWorld('getTime', getTime)
 contextBridge.exposeInMainWorld('getStatus', getStatus)
-contextBridge.exposeInMainWorld('axiou', axios)
-
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-
-
-
-
+contextBridge.exposeInMainWorld('axios', axios)
--- renderer.js
+++ renderer.js
@@ -1,8 +1,5 @@
-// DOM Manipulation in this file
 const main = document.getElementById('main');
-const x = './red-x.png';
-const check = './green-check.png';
-// access data from preload.js with contextBridge.exposeInMainWorld()
+
 const sites = Object.keys(localStorage)
 
 const addNewSite = () => {
@@ -16,52 +13,7 @@ const addNewSite = () => {
 for (site in sites) {
   let url = JSON.parse(localStorage.getItem(sites[site]))
   getStatus(url).then(res => {
-    const statusCard = document.createElement('div');
-    const id = url.name.replace('.', '-');
-    statusCard.setAttribute('id', id);
-    statusCard.classList.add('status-card');
-    const status = res.status;
-    const statusName = statusCodes.find(code => code.code === status).name;
-    const siteName = url.name;
-    const siteUrl = url.url;
-    const topDiv = document.createElement('div');
-    topDiv.classList.add('top-div');
-    const img = document.createElement('img');
-    img.src = status === 200 ? check : x;
-    img.alt = statusName;
-    topDiv.appendChild(img);
-    const topText = document.createElement('p');
-    topText.classList.add('top-text');
-    textLink = document.createElement('a');
-    textLink.href = siteUrl;
-    textLink.innerText = siteName;
-    textLink.target = '_blank';
-    topText.appendChild(textLink);
-    topDiv.appendChild(topText);
-    const bottomDiv = document.createElement('div');
-    bottomDiv.classList.add('bottom-div');
-    const bottomText = document.createElement('p');
-    bottomText.classList.add('bottom-text');
-    bottomText.innerText = `${siteUrl} - ${status} - ${statusName}`;
-    bottomDiv.appendChild(bottomText)
-    const infoDiv = document.createElement('div');
-    infoDiv.classList.add('info-div');
-    const btnDiv = document.createElement('div');
-    btnDiv.classList.add('btn-div');
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('delete-btn');
-    deleteBtn.innerText = 'Remove';
-    deleteBtn.addEventListener('click', () => {
-      localStorage.removeItem(siteName);
-      statusCard.remove();
-    })
-    btnDiv.appendChild(deleteBtn);
-    infoDiv.appendChild(topDiv);
-    infoDiv.appendChild(bottomDiv);
-    statusCard.appendChild(infoDiv);
-    statusCard.appendChild(btnDiv)
-    main.appendChild(statusCard);
-    
+    createCard(res, url)
   })
 }
 
